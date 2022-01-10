PedestrianSean/ObjC-Lua

commit 1f1008b231ff1a13cf3fedb96456c16ee6809f24

Adds LuaContext.parseResult property for retrieving a single return value when parsing (and running) a script.

classes/LuaContext.m
@@ -51,6 +51,7 @@ static const struct luaL_Reg luaWrapperMetaFunctions[] = {
     lua_State *L;
     NSMutableDictionary *_exportedClasses;
 }
+@property (strong) id parseResult;
 @end
 
 static int luaPanicked(lua_State *L) {
@@ -102,16 +103,27 @@ static const luaL_Reg loadedlibs[] = {
         lua_close(L);
 }
 
-- (BOOL)parse:(NSString *)script error:(NSError *__autoreleasing *)error {
-    int result = luaL_dostring(L, [script UTF8String]);
-    if( result == LUA_OK )
-        return YES;
+- (BOOL)parse2:(int)result error:(NSError *__autoreleasing *)error {
+    if( result == LUA_OK ) {
+    	result = lua_pcall(L, 0, 1, 0);
+        if( result == LUA_OK ) {
+            self.parseResult = toObjC(L, -1);
+        } else {
+            self.parseResult = nil;
             if( error ) {
                 *error = [NSError errorWithDomain:LuaErrorDomain
                                              code:result
                                          userInfo:@{ NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Could not parse script: %s", lua_tostring(L,-1)] }];
             }
-    return NO;
+        }
+        lua_pop(L, 1);
+    }
+    return result == LUA_OK;
+}
+
+- (BOOL)parse:(NSString *)script error:(NSError *__autoreleasing *)error {
+    int result = luaL_loadstring(L, [script UTF8String]);
+    return [self parse2:result error:error];
 }
 
 - (BOOL)parseURL:(NSURL *)url error:(NSError *__autoreleasing *)error {
@@ -122,15 +134,8 @@ static const luaL_Reg loadedlibs[] = {
                                      userInfo:@{ NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Invalid script path '%@'", url] }];
         return NO;
     }
-    int result = luaL_dofile(L, [[url path] UTF8String]);
-    if( result == LUA_OK )
-        return YES;
-    if( error ) {
-        *error = [NSError errorWithDomain:LuaErrorDomain
-                                     code:result
-                                 userInfo:@{ NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Could not parse script: %s", lua_tostring(L,-1)] }];
-    }
-    return NO;
+    int result = luaL_loadfile(L, [[url path] UTF8String]);
+    return [self parse2:result error:error];
 }
 
 - (BOOL)fromObjC:(id)object {

classes/LuaContext.h
@@ -48,12 +48,17 @@ typedef NS_ENUM(NSUInteger, LuaErrorCode) {
  `LuaContext` contains all the methods needed to parse & call Lua scripts. See the top-level overview for examples.
 
  The call:with:error:, objectForKeyedSubscript:, and setObject:forKeyedSubscript: methods will automatically translate
- between NSNull, NSNumber, NSString, NSArray, and NSDictionary types and their Lua equivelents. If you need to pass
+ between NSNull, NSNumber, NSString, NSArray, and NSDictionary types and their Lua equivalents. If you need to pass
  other object types into the context, you must use the LuaExport protocol.
 
  The context will also translate between NSValue types containing CGPoint, CGSize, CGRect, CGAffineTransform, and CATransform3D structs.
  */
 @interface LuaContext : NSObject
+
+/**
+ `parseResult` contains the single-value result from the last `parse:error:` or `parseURL:error:` invocation
+ */
+@property (readonly) id parseResult;
 
 /**
  @param script A Lua script to be parsed and added to this context

commit cb4e16db71de372086a0f2e345035c4e5a2a53a2

Fixes a bug introduced by commit 1f1008b2 when refactoring the `parse:` and `parseURL:` functions

classes/LuaContext.m
@@ -134,19 +134,20 @@ static const luaL_Reg loadedlibs[] = {
                 #endif
                 _parseResult = newv;
             }
-        } else {
-            #if ! __has_feature(objc_arc)
-                [_parseResult release];
-            #endif
-            _parseResult = nil;
-            if( error ) {
-                *error = [NSError errorWithDomain:LuaErrorDomain
-                                             code:result
-                                         userInfo:@{ NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Could not parse script: %s", lua_tostring(L,-1)] }];
-            }
         }
-        lua_pop(L, 1);
     }
+    if( result != LUA_OK ) {
+        #if ! __has_feature(objc_arc)
+            [_parseResult release];
+        #endif
+        _parseResult = nil;
+        if( error ) {
+            *error = [NSError errorWithDomain:LuaErrorDomain
+                                         code:result
+                                     userInfo:@{ NSLocalizedDescriptionKey: [NSString stringWithFormat:@"Could not parse script: %s", lua_tostring(L,-1)] }];
+        }
+    }
+    lua_pop(L, 1);
     return result == LUA_OK;
 }
 